1712964f252c22983896c0dac2889e47e0a45a69
Thanh Ngo <felix.infinite@gmail.com>
2017-10-29 02:05:36 -0400

    Fix off by one problem

--- Assignment 4/client/src/main/java/Client.java
+++ Assignment 4/client/src/main/java/Client.java
@@ -1,125 +1,131 @@
-import java.io.*;
-import java.net.*;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 
 public class Client {
 
-	private static final int CLIENT_PORT_NUMBER = 12345;
+    private static final int CLIENT_PORT_NUMBER = 12345;
 
 
-	private static int TOTAL_SIZE_TO_SEND = 1 << 30; // 2^30 don't change
-	private static double BYTES_IN_MB = 1 << 20;
-	private static String serverHost ;
-	private static int serverPort;
-	private static String protocol;
-	private static int ack;
-	private static int msg_size;
-	private static int bytes_count;
+    private static int TOTAL_SIZE_TO_SEND = 1 << 30; // 2^30 don't change
+    private static double BYTES_IN_MB = 1 << 20;
+    private static String serverHost;
+    private static int serverPort;
+    private static String protocol;
+    private static int ack;
+    private static int msg_size;
+    private static int bytes_count;
 
-	private static int msg_count = 0;
-	private static void fatal(String err){
-		System.err.println(err);
-		System.exit(1);
-	}
+    private static int msg_count = 0;
 
-	// the client sends out new-line-delimited byte chunk
-	public static void main (String args[]) throws Exception {
+    private static void fatal(String err) {
+        System.err.println(err);
+        System.exit(1);
+    }
 
-		if (args.length != 5){
-			fatal("Error: Client needs 5 arguments\n" +
-					"\tUsage: ./client serverHost serverPort protocol ack messageSize");
-		}
-		serverHost = args[0];
-		serverPort = Integer.parseInt(args[1]);
-		protocol = args[2];
-		ack = Integer.parseInt(args[3]);
-		if (ack != 0 && ack != 1){
-			fatal("Error: Acknowledgement argument should be either 0 or 1");
-		}
-		msg_size = Integer.parseInt(args[4]);
-		if(msg_size <= 0 || msg_size > 65536){
-			fatal("Error: Message size argument should be in the range of [1,65536]");
-		}
+    // the client sends out new-line-delimited byte chunk
+    public static void main(String args[]) throws Exception {
 
-		long startTime = System.currentTimeMillis();
-		if (protocol.equals("tcp")){
-			Socket socket = new Socket(serverHost, serverPort);
-			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-			DataInputStream in = new DataInputStream(socket.getInputStream());
-			out.writeInt(ack); // sends ack info
-			out.writeInt(msg_size); // sends message size
-			out.flush();
-			msg_count += 1;
-			bytes_count += 8;
-			// get ack for the initial packet
-			if(ack == 1){
-				in.readByte();
-			}
+        if (args.length != 5) {
+            fatal("Error: Client needs 5 arguments\n" +
+                    "\tUsage: ./client serverHost serverPort protocol ack messageSize");
+        }
+        serverHost = args[0];
+        serverPort = Integer.parseInt(args[1]);
+        protocol = args[2];
+        ack = Integer.parseInt(args[3]);
+        if (ack != 0 && ack != 1) {
+            fatal("Error: Acknowledgement argument should be either 0 or 1");
+        }
+        msg_size = Integer.parseInt(args[4]);
+        if (msg_size <= 0 || msg_size > 65536) {
+            fatal("Error: Message size argument should be in the range of [1,65536]");
+        }
 
-			int count = TOTAL_SIZE_TO_SEND;
-			while(count > 0){
-				byte[] sent = new byte[msg_size];
-				out.write(sent);
-				out.flush();
-				if (ack == 1){
-					in.readByte();
-				}
-				count -= msg_size;
-				msg_count += 1;
-				bytes_count += msg_size;
-			}
-		}else if (protocol.equals("udp")){
+        long startTime = System.currentTimeMillis();
+        if (protocol.equals("tcp")) {
+            Socket socket = new Socket(serverHost, serverPort);
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            out.writeInt(ack); // sends ack info
+            out.writeInt(msg_size); // sends message size
+            out.flush();
+            msg_count += 1;
+            bytes_count += 8;
+            // get ack for the initial packet
+            if (ack == 1) {
+                in.readByte();
+            }
 
-			// Set up a socket
-			DatagramSocket socket = new DatagramSocket(CLIENT_PORT_NUMBER);
+            int count = TOTAL_SIZE_TO_SEND;
+            while (count > 0) {
+                byte[] sent = new byte[msg_size];
+                out.write(sent);
+                out.flush();
+                if (ack == 1) {
+                    in.readByte();
+                }
+                count -= msg_size;
+                msg_count += 1;
+                bytes_count += msg_size;
+            }
+        } else if (protocol.equals("udp")) {
 
-			// Construct the initial packet
-			ByteArrayOutputStream initBytes = new ByteArrayOutputStream();
-			DataOutputStream initWriter = new DataOutputStream(initBytes);
-			initWriter.writeInt(ack);
-			initWriter.writeInt(msg_size);
-			initWriter.flush();
-			byte[] initData = initBytes.toByteArray();
-			DatagramPacket initPacket = new DatagramPacket(initData,initData.length,new InetSocketAddress
-					(serverHost,serverPort));
+            // Set up a socket
+            DatagramSocket socket = new DatagramSocket(CLIENT_PORT_NUMBER);
 
-			// Send the init packet
-			socket.send(initPacket);
-			msg_count += 1;
-			bytes_count += initData.length;
+            // Construct the initial packet
+            ByteArrayOutputStream initBytes = new ByteArrayOutputStream();
+            DataOutputStream initWriter = new DataOutputStream(initBytes);
+            initWriter.writeInt(ack);
+            initWriter.writeInt(msg_size);
+            initWriter.flush();
+            byte[] initData = initBytes.toByteArray();
+            DatagramPacket initPacket = new DatagramPacket(initData, initData.length, new InetSocketAddress
+                    (serverHost, serverPort));
 
-			// wait for acknowledgement
-			if(ack == 1){
-				DatagramPacket initRes = new DatagramPacket(new byte[1], 0);
-				socket.receive(initRes);
-			}
+            // Send the init packet
+            socket.send(initPacket);
+            msg_count += 1;
+            bytes_count += 8;
 
-			int count = TOTAL_SIZE_TO_SEND;
-			while(count > 0 ){
-				byte[] sent = new byte[msg_size];
-				DatagramPacket packet = new DatagramPacket(sent,sent.length,new InetSocketAddress
-						(serverHost,serverPort));
-				socket.send(packet);
-				if (ack == 1){
-					DatagramPacket packetAck = new DatagramPacket(new byte[1], 1);
-					socket.receive(packetAck);
-				}
-				count -= msg_size;
-				bytes_count += sent.length;
-				msg_count += 1;
-			}
-		}else{
-			fatal("Error: The protocol is not supported");
-		}
-		long stopTime = System.currentTimeMillis();
-		long elapsed = stopTime - startTime;
-		double rate = ((double)bytes_count)/BYTES_IN_MB/((double) elapsed) *1000d;
-		// Number of messages sent
-		// Number of bytes sent
-		// Total transmit time
-		System.out.println("Number of messages sent: "+ msg_count+" messages.");
-		System.out.println("Number of bytes sent: "+bytes_count+" bytes.");
-		System.out.println("Total transmit time: "+ elapsed+" milliseconds.");
-		System.out.println("Rate of transmission: "+ Math.round(rate)+ " Mbps." );
-	}
+            // wait for acknowledgement
+            if (ack == 1) {
+                DatagramPacket initRes = new DatagramPacket(new byte[1], 1);
+                socket.receive(initRes);
+            }
+
+            int count = TOTAL_SIZE_TO_SEND;
+            while (count > 0) {
+                byte[] sent = new byte[msg_size];
+                DatagramPacket packet = new DatagramPacket(sent, msg_size, new InetSocketAddress
+                        (serverHost, serverPort));
+                socket.send(packet);
+                if (ack == 1) {
+                    DatagramPacket packetAck = new DatagramPacket(new byte[1], 1);
+                    socket.receive(packetAck);
+                }
+                count -= sent.length;
+                bytes_count += sent.length;
+                msg_count += 1;
+            }
+        } else {
+            fatal("Error: The protocol is not supported");
+        }
+        long stopTime = System.currentTimeMillis();
+        long elapsed = stopTime - startTime;
+        double rate = ((double) bytes_count) / BYTES_IN_MB / ((double) elapsed) * 1000d;
+        // Number of messages sent
+        // Number of bytes sent
+        // Total transmit time
+        System.out.println("Number of messages sent: " + msg_count + " messages.");
+        System.out.println("Number of bytes sent: " + bytes_count + " bytes.");
+        System.out.println("Total transmit time: " + elapsed + " milliseconds.");
+        System.out.println("Rate of transmission: " + Math.round(rate) + " Mbps.");
+    }
 
 }
--- Assignment 4/server/src/main/java/Server.java
+++ Assignment 4/server/src/main/java/Server.java
@@ -1,139 +1,172 @@
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.*;
 
 
 public class Server {
-	//this is a per-thread copy of the client socket
-	Socket conn;
-
-	//if we defined this static, then it would be shared among threads
-	private static ServerSocket svc;
-
-	Server(Socket sock) {
-		this.conn = sock;	//store the socket for the connection
-	}
 
 
-	private static void fatal(String err){
-		System.err.println(err);
-		System.exit(1);
-	}
+    private static void fatal(String err) {
+        System.err.println(err);
+        System.exit(1);
+    }
 
-	private static final int INIT_UDP_PACKET_SIZE = 20; //bytes
+    private static final int INIT_UDP_PACKET_SIZE = 8; //bytes
 
-	private static DatagramPacket makeAckPacket(SocketAddress client){
-		return new DatagramPacket(new byte[1], 1, client);
-	}
+    private static DatagramPacket makeAckPacket(SocketAddress client) {
+        return new DatagramPacket(new byte[1], 1, client);
+    }
 
-	private static void udpServer(int port) throws  SocketException{
+    private static void udpServer(int port) throws SocketException {
 
-		while(true) {
-			int ack;
-			int msg_size;
-			SocketAddress clientAddr;
-			try {
-				// Make a server socket
-				DatagramSocket socket = new DatagramSocket(port);
-				// Receive the init packet
-				DatagramPacket initPacket = new DatagramPacket(new byte[INIT_UDP_PACKET_SIZE], INIT_UDP_PACKET_SIZE);
-				socket.receive(initPacket);
-
-				// Get client addr
-				clientAddr = initPacket.getSocketAddress();
-
-				// Read from the init packet
-				ByteArrayInputStream initBytes = new ByteArrayInputStream(initPacket.getData());
-				DataInputStream initReader = new DataInputStream(initBytes);
-				ack = initReader.readInt();
-				msg_size = initReader.readInt();
-				initReader.close();
-
-				if (ack == 1) {
-					socket.send(makeAckPacket(clientAddr));
-				}
-
-				int count = 0;
-				while (true) {
-					DatagramPacket received = new DatagramPacket(new byte[msg_size], msg_size);
-					socket.receive(received);
-					count += received.getLength();
-					if (ack == 1) {
-						socket.send(makeAckPacket(clientAddr));
-					}
-					if (count >= TOTAL_SIZE_TO_SEND) {
-						break;
-					}
-				}
-				socket.close();
-			} catch (IOException e) {
-				System.err.println("udp server: receive init packet: " + e.getMessage());
-			}
-		}
-	}
+        while (true) {
+            int ack;
+            int msg_size;
+            int bytes_read = 0;
+            int msg_read = 0;
+            SocketAddress clientAddr;
+            try {
+                // Make a server socket
+                DatagramSocket socket = new DatagramSocket(port);
+                // Receive the init packet
+                DatagramPacket initPacket = new DatagramPacket(new byte[INIT_UDP_PACKET_SIZE], INIT_UDP_PACKET_SIZE);
+                socket.receive(initPacket);
 
 
-	private static int TOTAL_SIZE_TO_SEND = 1 << 30; // 2^30 don't change
+                // Get client addr
+                clientAddr = initPacket.getSocketAddress();
 
-	private static  void handleTCPClient(Socket socket){
-		int ack;
-		int msg_size;
-		try {
-			DataInputStream reader = new DataInputStream(socket.getInputStream());
-			DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
-			ack = reader.readInt();
-			msg_size = reader.readInt();
-			if (ack == 1){
-				writer.writeByte(0);
-				writer.flush();
-			}
-			int count = 0;
-			while(true){
-				byte[] received = new byte[msg_size];
-				int temp = reader.read(received);
-				count += temp;
-				if(ack == 1){
-					writer.writeByte(0);
-					writer.flush();
-				}
-				if (count >=TOTAL_SIZE_TO_SEND){
-					break;
-				}
-			}
-			reader.close();
-			writer.close();
-			socket.close();
-		}catch (IOException e){
-			System.err.println("tcp client: error: "+ e.getMessage());
-			return;
-		}
-	}
+                // Read from the init packet
+                ByteArrayInputStream initBytes = new ByteArrayInputStream(initPacket.getData());
+                DataInputStream initReader = new DataInputStream(initBytes);
+                ack = initReader.readInt();
+                if (ack == 0){
+                    System.out.println("ERR: UDP Streaming selected. Results will not be printed if the " +
+                            "data (1GB) is not sent entirely. Please restart the server for each run in this " +
+                            "configuration." +
+                            ".");
+                }
+                msg_size = initReader.readInt();
+                initReader.close();
+                msg_read += 1;
+                bytes_read += initPacket.getData().length;
 
-	private static void tcpServer(int port){
-		try {
-			ServerSocket serverSocket = new ServerSocket(port, 5);
-			while(true){
-				Socket sock = serverSocket.accept();
-				new Thread(()->handleTCPClient(sock)).start();
-			}
-		}catch (IOException e){
-			fatal("tcp server: "+ e.getMessage());
-		}
-	}
+                // ACK the init packet
+                if (ack == 1) {
+                    socket.send(makeAckPacket(clientAddr));
+                }
 
-	private static int tcpPort;
+                int count = 0;
+                while (true) {
+                    DatagramPacket received = new DatagramPacket(new byte[msg_size], msg_size);
+                    socket.receive(received);
+                    msg_read += 1;
+                    bytes_read += received.getLength();
+                    count += received.getLength();
+                    if (ack == 1) {
+                        socket.send(makeAckPacket(clientAddr));
+                    }
+                    if (count >= TOTAL_SIZE_TO_SEND) {
+                        break;
+                    }
+                }
+                if (ack == 0) {
+                    System.out.println("Acknowledgement and protocol used: Pure streaming UDP");
+                } else if (ack == 1) {
+                    System.out.println("Acknowledgement and protocol used: Stop-and-wait UDP");
+                }
+                System.out.println("Number of messages read: " + msg_read + " messages.");
+                System.out.println("Number of bytes read: " + bytes_read + " bytes.");
+                socket.close();
 
-	private static int udpPort;
+            } catch (IOException e) {
+                System.err.println("udp server: receive init packet: " + e.getMessage());
+            }
+        }
+    }
 
-	public static void main(String args[]) throws Exception {
-		if (args.length != 2){
-			fatal("Server: server needs 2 arguments " +
-					"\n" +
-					"\tUsage: ./server tcpPort udpPort");
-		}
-		tcpPort = Integer.parseInt(args[0]);
-		udpPort = Integer.parseInt(args[1]);
 
-		new Thread(()-> tcpServer(tcpPort)).start();
-		udpServer(udpPort);
-	}
+    private static int TOTAL_SIZE_TO_SEND = 1 << 30; // 2^30 don't change
+
+    private static void handleTCPClient(Socket socket) {
+        int ack = 0;
+        int msg_size = 0;
+        int msg_read = 0;
+        int bytes_read = 0;
+        try {
+            // Get and process the first message
+            DataInputStream reader = new DataInputStream(socket.getInputStream());
+            DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
+            ack = reader.readInt();
+            msg_size = reader.readInt();
+            msg_read += 1;
+            bytes_read += 8;
+            // ACK the first one
+            if (ack == 1) {
+                writer.writeByte(0);
+                writer.flush();
+            }
+
+            int count = 0;
+            while (true) {
+                byte[] received = new byte[msg_size];
+                int hasRead = reader.read(received, 0, msg_size);
+                count += hasRead;
+                msg_read += 1;
+                bytes_read += hasRead;
+
+                if (ack == 1) {
+                    writer.writeByte(0);
+                    writer.flush();
+                }
+                if (count >= TOTAL_SIZE_TO_SEND) {
+                    break;
+                }
+            }
+            reader.close();
+            writer.close();
+            socket.close();
+            if (ack == 0) {
+                System.out.println("Acknowledgement and protocol used: Pure streaming TCP");
+            } else if (ack == 1) {
+                System.out.println("Acknowledgement and protocol used: Stop-and-wait TCP");
+            }
+            System.out.println("Number of messages read: " + msg_read + " messages.");
+            System.out.println("Number of bytes read: " + bytes_read + " bytes.");
+        } catch (IOException e) {
+            System.err.println("tcp client: error: " + e.getMessage());
+            return;
+        }
+    }
+
+    private static void tcpServer(int port) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port, 5);
+            while (true) {
+                Socket sock = serverSocket.accept();
+                new Thread(() -> handleTCPClient(sock)).start();
+            }
+        } catch (IOException e) {
+            fatal("tcp server: " + e.getMessage());
+        }
+    }
+
+    private static int tcpPort;
+
+    private static int udpPort;
+
+    public static void main(String args[]) throws Exception {
+        if (args.length != 2) {
+            fatal("Server: server needs 2 arguments " +
+                    "\n" +
+                    "\tUsage: ./server tcpPort udpPort");
+        }
+        tcpPort = Integer.parseInt(args[0]);
+        udpPort = Integer.parseInt(args[1]);
+
+        new Thread(() -> tcpServer(tcpPort)).start();
+        udpServer(udpPort);
+    }
 }
